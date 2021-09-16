81cdb5ecd066cb90eefa4daec002f65213c1c8b9
Soroush Javed Sulehri <saroushjaved@gmail.com>
2021-09-16 19:26:53 +0500

    //cahnges

--- assets/js/global.js
+++ assets/js/global.js
@@ -54,3 +54,20 @@
     });
   });
 }(jQuery));
+
+
+$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<3;i++) {
+    next=next.next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
