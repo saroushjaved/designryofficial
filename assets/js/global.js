c10ba4188d3bf2bad21501bfaa0fe888e864570d
Soroush Javed Sulehri <saroushjaved@gmail.com>
2021-11-06 17:47:47 +0500

    //for example

--- assets/js/global.js
+++ assets/js/global.js
@@ -56,18 +56,3 @@
 }(jQuery));
 
 
-$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-
-  for (var i=0;i<3;i++) {
-    next=next.next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-  }
-});
